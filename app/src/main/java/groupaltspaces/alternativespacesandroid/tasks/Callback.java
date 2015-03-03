86a1b8e0b710f9c189becaa063c5a49e7d1cc3a7
Jonas Therkelsen <jonas.foyn@gmail.com>
2015-03-02 16:55:18 -0800

    new version

--- app/src/main/java/groupaltspaces/alternativespacesandroid/tasks/Callback.java
+++
@@ -1,9 +0,0 @@
-package groupaltspaces.alternativespacesandroid.tasks;
-
-import java.util.List;
-
-public interface Callback {
-    public void onSuccess();
-
-    public void onFail(List<String> message);
-}
--- app/src/main/java/groupaltspaces/alternativespacesandroid/tasks/InterestCallback.java
+++
@@ -1,8 +0,0 @@
-package groupaltspaces.alternativespacesandroid.tasks;
-
-import java.util.List;
-import java.util.Map;
-
-public interface InterestCallback {
-    public void onInterestReceived(List<Map<String, Object>> map);
-}
