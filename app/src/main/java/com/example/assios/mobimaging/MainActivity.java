8bee0d2b1b8f2735d2693a1bd86f683e4c1654d4
Asbjørn Steinskog <asbjorn@steinskog.me>
2015-03-04 18:35:03 -0800

    Create OpenCV camera view

--- app/src/main/java/com/example/assios/mobimaging/MainActivity.java
+++ app/src/main/java/com/example/assios/mobimaging/MainActivity.java
@@ -2,24 +2,71 @@ package com.example.assios.mobimaging;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import org.opencv.android.BaseLoaderCallback;
+import android.view.SurfaceView;
 
-public class MainActivity extends ActionBarActivity {
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+public class MainActivity extends ActionBarActivity implements CvCameraViewListener2 {
+
+    private final static String TAG = "OpenCvTest:MainActivity";
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch(status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                    break;
+                }
+                default:
+                {
+                    super.onManagerConnected(status);
+                }
+            }
+        }
+    };
+
+    private JavaCameraView mOpenCvCameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        mOpenCvCameraView = (JavaCameraView) findViewById(R.id.MainActivityCameraView);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, this, mLoaderCallback);
+
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null) {
+            mOpenCvCameraView.disableView();
+        }
     }
 
     @Override
@@ -35,5 +82,21 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //CvCameraViewListener2 stuff
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        // TODO stuff
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        // TODO stuff
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return inputFrame.rgba();
     }
 }
