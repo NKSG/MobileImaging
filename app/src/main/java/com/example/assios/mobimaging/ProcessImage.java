43ff5b9f69f4b29b4002547addbd116e0606d5c6
Asbjørn Steinskog <asbjorn@steinskog.me>
2015-03-13 12:30:45 -0700

    Updates

--- app/src/main/java/com/example/assios/mobimaging/ProcessImage.java
+++ app/src/main/java/com/example/assios/mobimaging/ProcessImage.java
@@ -1,5 +1,7 @@
 package com.example.assios.mobimaging;
 
+import android.util.Log;
+
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
@@ -19,20 +21,45 @@ import org.opencv.imgproc.Imgproc;
  */
 public class ProcessImage {
 
+    public static int[][] zeros (Size size) {
+        int width = (int) size.width;
+        int height = (int) size.height;
+
+        int[][] output = new int[width][height];
+
+        for (int i = 0; i < width; i++)
+            for (int j = 0; j < height; j++) {
+                output[i][j] = 0;
+            }
+
+        return output;
+    }
+
+    public static float[][] f (float[][] gray, )
+
     public static Mat image(Mat input) {
+
+        Mat blur = new Mat();
+        Mat gray = new Mat();
+        Mat close = new Mat();
+        int[][] mask;
 
         int tall = 10;
 
         //Image preprocessing
-        Imgproc.GaussianBlur(input, input, new Size(5, 5), 100);
+        Imgproc.GaussianBlur(input, blur, new Size(5, 5), 100);
 
-        Imgproc.cvtColor(input, input, Imgproc.COLOR_RGB2GRAY);
+        Imgproc.cvtColor(blur, gray, Imgproc.COLOR_RGB2GRAY);
 
-        // LEGG INN mask = np.zeros((gray.shape),np.uint8) HER
+        mask = zeros(gray.size());
+
+        System.out.println("HALLA");
 
         Mat kernel1 = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(11, 11));
 
-        Imgproc.morphologyEx(input, input, Imgproc.MORPH_CLOSE, kernel1);
+        Imgproc.morphologyEx(gray, close, Imgproc.MORPH_CLOSE, kernel1);
+
+        Log.i("yo", "Graysize: " + gray.size());
 
         return input;
 
