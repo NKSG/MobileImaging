369c419e6b28ddda3b1c813103bae20a05d54288
Jonas Therkelsen <jonas.foyn@gmail.com>
2015-03-13 14:31:01 -0700

    mat of float etc

--- app/src/main/java/com/example/assios/mobimaging/ProcessImage.java
+++ app/src/main/java/com/example/assios/mobimaging/ProcessImage.java
@@ -9,7 +9,10 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Size;
 import org.opencv.core.MatOfPoint2f;
@@ -35,33 +38,58 @@ public class ProcessImage {
         return output;
     }
 
-    public static float[][] f (float[][] gray, )
+    public static float[][] f (float[][] gray) {
+       return null;
+    }
+
+//    public Mat toFloat(Mat board) {
+//        for (int i=0; i<board.height(); i++ ) {
+//            for (int j=0; j<board.width(); j++) {
+//                for (Double d :board.get(i,j)) {
+//                    float temp;
+//                }
+//                board.put(i,j, board.get(i,j));
+//            }
+//        }
+//    }
 
     public static Mat image(Mat input) {
 
-        Mat blur = new Mat();
-        Mat gray = new Mat();
-        Mat close = new Mat();
-        int[][] mask;
+        Mat blur = new MatOfFloat();
+        Mat mask = new MatOfFloat();
 
-        int tall = 10;
+        MatOfFloat gray = new MatOfFloat();
+        MatOfFloat close = new MatOfFloat();
 
         //Image preprocessing
-        Imgproc.GaussianBlur(input, blur, new Size(5, 5), 100);
+        Imgproc.GaussianBlur(input, blur, new Size(5.0, 5.0), 100.0);
 
         Imgproc.cvtColor(blur, gray, Imgproc.COLOR_RGB2GRAY);
 
-        mask = zeros(gray.size());
+//        mask = zeros(gray.size());
 
-        System.out.println("HALLA");
+        mask = Mat.zeros(gray.size(), gray.type());
 
-        Mat kernel1 = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(11, 11));
-
+        Mat kernel1 = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(11.0, 11.0));
         Imgproc.morphologyEx(gray, close, Imgproc.MORPH_CLOSE, kernel1);
 
-        Log.i("yo", "Graysize: " + gray.size());
 
-        return input;
+
+        Mat div = new MatOfFloat();
+        Core.divide(gray, close, div);
+
+
+
+        String dump = gray.dump();
+        Log.d("Here comes the string: ", dump);
+
+        Mat res = new MatOfFloat();
+        Core.normalize(div, res, 0, 255, Core.NORM_MINMAX);
+
+        Mat res2 = new MatOfFloat();
+        Imgproc.cvtColor(res, res2, Imgproc.COLOR_GRAY2BGR);
+
+        return res2;
 
     }
 
